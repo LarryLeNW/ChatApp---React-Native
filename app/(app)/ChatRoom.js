2b8605506fe9b4ba5b17f28413e638de4f9e1941
larrylenw <larrylenw@gmail.com>
2024-08-27 23:59:38 +0700

    handle create message & rom

--- app/(app)/ChatRoom.js
+++ app/(app)/ChatRoom.js
@@ -1,19 +1,78 @@
-import { Feather } from "@expo/vector-icons";
+import { Feather, FontAwesome } from "@expo/vector-icons";
 import { Image } from "expo-image";
 import { Stack, useLocalSearchParams, useRouter } from "expo-router";
-import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { heightPercentageToDP } from "react-native-responsive-screen";
+import MessageList from "./MessageList";
+import { useAuth } from "../../context/authContext";
+import { formatChatRoom } from "../../utils/common";
+import { addDoc, collection, doc, setDoc, Timestamp } from "firebase/firestore";
+import { db } from "../../config/firebase.config";
 
 const ChatRoom = () => {
     const item = useLocalSearchParams();
     const router = useRouter();
+    const { user } = useAuth(user);
+    const [messages, setMessages] = useState([]);
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        createRoomIfNotExit();
+    }, []);
+
+    const createRoomIfNotExit = async () => {
+        let roomId = formatChatRoom(user?.userId, item.userId);
+        await setDoc(doc(db, "rooms", roomId), {
+            roomId,
+        });
+        console.log("🚀 ~ createRoomIfNotExit ~ roomId:", roomId);
+    };
+
+    const handleSendMessage = async () => {
+        if (!message.trim()) {
+            Alert.alert("Please !!!", "Please enter a message");
+            return;
+        }
+
+        try {
+            const roomId = formatChatRoom(user.userId, item.userId);
+            const docRef = doc(db, "rooms", roomId);
+            const messageRef = collection(docRef, "messages");
+
+            const newDoc = await addDoc(messageRef, {
+                userId: user.userId,
+                text: message.trim(),
+                profileURL: user.profileURL,
+                senderName: user.username,
+                createdAt: Timestamp.fromDate(new Date()),
+            });
+
+            console.log("🚀 ~ handleSendMessage ~ newDoc:", newDoc.id);
+        } catch (error) {
+            console.log("🚀 ~ handleSendMessage ~ error:", error);
+        }
+    };
 
     return (
         <View>
             <Header infoConnect={item} router={router} />
-            <Text>ChatRoom with {item.username}</Text>
-            <Footer />
+            <View className="d-flex flex-col h-full">
+                <View className="h-[85%]">
+                    <MessageList />
+                </View>
+                <View className="h-[10%] d-flex flex-row justify-between items-center mx-4 bg-white border px-4 py-2 border-neutral-300 rounded-full ">
+                    <TextInput
+                        placeholder="Enter message..."
+                        className=" text-lg  w-[90%]  "
+                        value={message}
+                        onChangeText={(value) => setMessage(value)}
+                    />
+                    <TouchableOpacity onPress={handleSendMessage}>
+                        <FontAwesome name="send-o" size={28} color="blue" />
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 };
@@ -25,7 +84,7 @@ const Header = ({ infoConnect, router }) => {
                 title: "",
                 headerShadowVisible: false,
                 headerLeft: () => (
-                    <View className=" flex-row items-center gap-1 px-4 py-2">
+                    <View className=" flex-row items-center gap-1 px-4 py-2 ">
                         <TouchableOpacity onPress={() => router.back()}>
                             <Feather
                                 name="arrow-left"
@@ -73,14 +132,6 @@ const Header = ({ infoConnect, router }) => {
                 },
             }}
         />
-    );
-};
-
-const Footer = () => {
-    return (
-        <View>
-            <Text>Footer</Text>
-        </View>
     );
 };
 
